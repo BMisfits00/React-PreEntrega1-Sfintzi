6e182cdb9c7665c75813abe7e6c59c6f2689a3ad
Brian <brian.misfits@gmail.com>
2024-06-13 16:04:04 -0300

    modifico los archivos de diseño y componentes

--- src/components/BotonComponente.jsx
+++ src/components/BotonComponente.jsx
@@ -1,11 +1,12 @@
-export default function BotonComponente({nombre, color}){
-    const misEstilos ={
-        backgroundColor : color
-    }
+export default function BotonComponente({nombre}){
+    
+    // const misEstilos ={
+    //     backgroundColor : color
+    // }
 
     const handleClick = () => {
         console.log(`Estas en la seccion de ${nombre}`)
     }
 
-    return <button onClick={handleClick} style={misEstilos}>{nombre}</button>
+    return <button onClick={handleClick}>{nombre}</button>
 }
--- src/components/NavComponente.jsx
+++ src/components/NavComponente.jsx
@@ -1,20 +1,29 @@
 import './NavComponente.css'
 import BotonComponente from "./BotonComponente";
+import CartWidget from './CartWidget';
 
 export default function NavComponente(){
     return(<>
     <div className='barra'>
+        
+        <div className="grid-boton">
+        <BotonComponente nombre='About us'/>
+        </div>
+        
+        <div className="grid-boton">
+        <BotonComponente nombre='Products'/>
+        </div>
+        
+        <div className="grid-boton">
+        <BotonComponente nombre='Contact'/>
+        </div>
+        
         <div className="grid-titulo">
             <h1 className='titulo'>Tecno World</h1>
         </div>
-        <div className="grid-boton">
-        <BotonComponente nombre='About us' color='green'/>
-        </div>
-        <div className="grid-boton">
-        <BotonComponente nombre='Products' color='red'/>
-        </div>
-        <div className="grid-boton">
-        <BotonComponente nombre='Contact' color='blue'/>
+
+        <div className="grid-carrito">
+            <CartWidget/>
         </div>
 
     </div>
